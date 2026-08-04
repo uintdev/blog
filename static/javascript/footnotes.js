211b23a2ecb044b4b06f9e92c6046810efe010c9
uintdev <core@uint.dev>
2026-08-04 21:19:32 +0100

    Shorten script

--- static/javascript/footnotes.js
+++ static/javascript/footnotes.js
@@ -1,21 +1,12 @@
-// Footnote Backlinking
-document.addEventListener("DOMContentLoaded", (_event) => {
-  const references = document.getElementsByClassName("footnote-reference");
-
-  for (const reference of references) {
-    const link = reference.firstChild;
-    const id = link.getAttribute("href").slice(1);
-    link.setAttribute("id", `${id}_ref`);
+document.addEventListener("DOMContentLoaded", () => {
+  for (const ref of document.querySelectorAll(".footnote-reference a")) {
+    ref.id = `${ref.hash.slice(1)}_ref`;
   }
 
-  const footnotes = document.getElementsByClassName("footnote-definition");
-
-  for (const footnote of footnotes) {
-    const id = footnote.getAttribute("id");
-    const backReference = document.createElement("a");
-    backReference.setAttribute("href", `#${id}_ref`);
-    backReference.textContent = "↩\uFE0E";
-    const footnoteParagraph = footnote.getElementsByTagName("p")[0];
-    footnoteParagraph.append(backReference);
+  for (const footnote of document.querySelectorAll(".footnote-definition")) {
+    const back = document.createElement("a");
+    back.href = `#${footnote.id}_ref`;
+    back.textContent = "↩\uFE0E";
+    footnote.querySelector("p").append(back);
   }
 });
