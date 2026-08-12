5b3058cc6afd0b4adeeaadf41f963c1f359d8666
uintdev <core@uint.dev>
2026-08-12 22:24:37 +0100

    Zola 0.23.3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 main() {
-    ZOLA_VERSION=0.23.2
+    ZOLA_VERSION=0.23.3
 
     curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
     tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
