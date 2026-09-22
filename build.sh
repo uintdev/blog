1a823c4c49fc5087d3c20ccb39abc79471afac7e
uintdev <core@uint.dev>
2026-09-22 20:23:59 +0100

    General optimizations

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 main() {
     ZOLA_VERSION=0.23.6
 
-    curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-    tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
-
-    git submodule update --init --recursive
+    curl -fsSL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" | tar -xz zola
 
     ./zola build
 }
